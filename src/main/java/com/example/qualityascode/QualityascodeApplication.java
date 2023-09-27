d6c324358c9e5c0fdab771eaae7f8868b338d5d6
imbao <42200777+imbao@users.noreply.github.com>
2023-09-27 12:03:49 +0700

    Initial Commit

---
+++ src/main/java/com/example/qualityascode/QualityascodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.qualityascode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityascodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityascodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qualityascode/QualityascodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qualityascode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityascodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
