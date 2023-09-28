bde7b0022d2116e9bfb3539c9c68b33ee878dc9d
baohaicario <bao.ha@icariohealth.com>
2023-09-28 16:58:21 +0700

    test

--- src/main/java/com/example/qualityascode/greeting/HelloController.java
+++ src/main/java/com/example/qualityascode/greeting/HelloController.java
@@ -20,24 +20,24 @@ public class HelloController {
     return "Hello world";
   }
 
-  @RequestMapping("/create-sonar-project")
-  public String callRestfulAPI() {
-    RestTemplate restTemplate = new RestTemplate();
-    String uri = "https://sonarcloud.io/web_api/" + "/api/projects/create";
+  // @RequestMapping("/create-sonar-project")
+  // public ResponseEntity callRestfulAPI() {
+  //   RestTemplate restTemplate = new RestTemplate();
+  //   String uri = "https://sonarcloud.io/web_api/" + "/api/projects/create";
 
-    HttpHeader headers = new HttpHeaders();
-    // headers.setBearerAuth(token);
-    headers.setContentType(MediaType.APPLICATION_JSON);
+  //   HttpHeader headers = new HttpHeaders();
+  //   // headers.setBearerAuth(token);
+  //   headers.setContentType(MediaType.APPLICATION_JSON);
 
-    Map<String, Object> requestBody = new HashMap<>();
-    requestBody.put("name", "test_name");
-    requestBody.put("organization", "imbao");
-    requestBody.put("project", "imb_project");
+  //   Map<String, Object> requestBody = new HashMap<>();
+  //   requestBody.put("name", "test_name");
+  //   requestBody.put("organization", "imbao");
+  //   requestBody.put("project", "imb_project");
 
-    HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+  //   HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
 
-    ResponseEntity<Object> response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity,Object.class);
+  //   ResponseEntity<Object> response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity,Object.class);
 
-    return response;
-  }
+  //   return response;
+  // }
 }
